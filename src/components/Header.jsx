f3627fc37c6880b5ffed9307ff18cbd7789b42b3
SAMANDAR176 <samandartulanboyev26@gmail.com>
2025-07-10 16:39:44 +0500

    rasm qo'ydim

---
+++ src/components/Header.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+function Header() {
+  return (
+    <div>
+      <div className="tugmalar">
+        <button className="but">Home</button>
+        <button className="butt">Product</button>
+      </div>
+      <div className="rasm">
+        <img src="../imgs/Logo.svg" alt="" />
+      </div>
+      <div className="icon">
+        <img src="" alt="" />
+      </div>
+      <div className="tugma2">
+        <button></button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
